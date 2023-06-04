73ea5bb0db148ba5ee7368a301b66f90dc769ab4
ckc2007 <ckc2007@gmail.com>
2023-06-04 17:09:47 -0400

    making category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,20 +5,54 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    // be sure to include its associated Products
+    const categoryData = await Category.findAll({include: [Product]})
+  res.status(200).json(categoryData);
+  }
+  catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    })
+    res.status(200).json(categoryData)
+  }
+  catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory)
+  }
+  catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(categoryData)
+  }
+  catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
